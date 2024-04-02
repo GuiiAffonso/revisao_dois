d35591b0a5cc69061acd0455c01a293f79625247
GuiiAffonso <guiiaffonso.rh@gmail.com>
2024-04-02 20:31:18 -0300

    Cria a função somar

--- entrada_e_saida.py
+++ entrada_e_saida.py
@@ -1,5 +1,12 @@
-def nome():
-    nome = input("Qual o seu nome? ")
-    print(f'Olá {nome}! Seja bem vindo(a).')
-    
-nome()
+# def nome():
+#     nome = input("Qual o seu nome? ")
+#     print(f'Olá {nome}! Seja bem vindo(a).')
+
+# nome()
+
+def somar():
+    num1 = int(input("Digite o 1º número: "))
+    num2 = int(input("Digite o 2º número: "))
+    soma = num1 + num2
+    print(f'A soma dos números é = {soma}')
+somar()
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+def nome():
+#     nome = input("Qual o seu nome? ")
+#     print(f'Olá {nome}! Seja bem vindo(a).')
+
+# nome()
