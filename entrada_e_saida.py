d212807b7131c351accdb38b2e1a6d6ddcc795c8
GuiiAffonso <guiiaffonso.rh@gmail.com>
2024-04-02 20:39:40 -0300

    Resolve questão 4

--- entrada_e_saida.py
+++ entrada_e_saida.py
@@ -6,17 +6,25 @@
 # nome()
 
 # Questão 2:
-# def somar():
+# def somar_numeros():
 #     num1 = int(input("Digite o 1º número: "))
 #     num2 = int(input("Digite o 2º número: "))
 #     soma = num1 + num2
 #     print(f'A soma dos números é = {soma}')
-# somar()
+# somar_numeros()
 
 # Questão 3:
-def multiplica():
+# def multiplica_numeros():
+#     num1 = int(input("Digite o 1º número: "))
+#     num2 = int(input("Digite o 2º número: "))
+#     multiplicar = num1 * num2
+#     print(f'O produto dos números é: {multiplicar}')
+# multiplica_numeros()
+
+# Questão 4:
+def divide_numeros():
     num1 = int(input("Digite o 1º número: "))
     num2 = int(input("Digite o 2º número: "))
-    multiplicar = num1 * num2
-    print(f'O produto dos números é: {multiplicar}')
-multiplica()
+    dividir = num1 / num2
+    print(f'O produto dos números é: {dividir}')
+divide_numeros()
