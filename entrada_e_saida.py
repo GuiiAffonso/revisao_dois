b44988c5a5a6f639cb9b948fda43de0cf45c56ea
GuiiAffonso <guiiaffonso.rh@gmail.com>
2024-04-02 20:35:08 -0300

    Resolve questão 3

--- entrada_e_saida.py
+++ entrada_e_saida.py
@@ -1,12 +1,22 @@
+# Questão 1:
 # def nome():
 #     nome = input("Qual o seu nome? ")
 #     print(f'Olá {nome}! Seja bem vindo(a).')
 
 # nome()
 
-def somar():
+# Questão 2:
+# def somar():
+#     num1 = int(input("Digite o 1º número: "))
+#     num2 = int(input("Digite o 2º número: "))
+#     soma = num1 + num2
+#     print(f'A soma dos números é = {soma}')
+# somar()
+
+# Questão 3:
+def multiplica():
     num1 = int(input("Digite o 1º número: "))
     num2 = int(input("Digite o 2º número: "))
-    soma = num1 + num2
-    print(f'A soma dos números é = {soma}')
-somar()
+    multiplicar = num1 * num2
+    print(f'O produto dos números é: {multiplicar}')
+multiplica()
