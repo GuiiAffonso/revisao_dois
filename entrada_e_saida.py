1e327b68802df0f79ac7b97e8bab12de9a3bfa54
GuiiAffonso <guiiaffonso.rh@gmail.com>
2024-04-02 20:56:33 -0300

    Resolve questão 5

--- entrada_e_saida.py
+++ entrada_e_saida.py
@@ -22,9 +22,18 @@
 # multiplica_numeros()
 
 # Questão 4:
-def divide_numeros():
-    num1 = int(input("Digite o 1º número: "))
-    num2 = int(input("Digite o 2º número: "))
-    dividir = num1 / num2
-    print(f'O produto dos números é: {dividir}')
-divide_numeros()
+# def divide_numeros():
+#     num1 = int(input("Digite o 1º número: "))
+#     num2 = int(input("Digite o 2º número: "))
+#     dividir = num1 / num2
+#     print(f'O produto dos números é: {dividir}')
+# divide_numeros()
+
+# Questão 5:
+def calcular_imc():
+    altura = float(input("Digite sua altura: "))
+    peso = float(input("Digite o seu peso: "))
+    calcula_imc = peso / (altura*altura)
+    print(f'O seu IMC é: {calcula_imc})
+
+calcular_imc()
