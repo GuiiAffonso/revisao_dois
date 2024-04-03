5de8cbdf2a791a20a7e0dbeed0ac1ed7ab54e8d2
GuiiAffonso <guiiaffonso.rh@gmail.com>
2024-04-02 21:13:38 -0300

    Resolve questão 7

--- condicionais.py
+++ condicionais.py
@@ -1,10 +1,21 @@
 # Questão 6
-def verificar_maioridade():
-    idade = int(input("Digite sua idade: "))
+# def verificar_maioridade():
+#     idade = int(input("Digite sua idade: "))
 
-    if (idade >= 18):
-        print("Maior de idade.")
+#     if (idade >= 18):
+#         print("Maior de idade.")
+#     else:
+#         print("Menor de idade.")
+
+# verificar_maioridade()
+
+# Questão 7:
+def verificar_par_ou_impar():
+    numero = int(input("Digite um nº: "))
+
+    if (numero % 2 == 0):
+        print("Este é um nº par.")
     else:
-        print("Menor de idade.")
+        print("Este é um nº ímpar.")
 
-verificar_maioridade()
+verificar_par_ou_impar()
