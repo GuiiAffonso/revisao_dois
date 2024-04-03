2df22b5005d7c50c1885ec4fd28d877ebc4c1168
GuiiAffonso <guiiaffonso.rh@gmail.com>
2024-04-02 21:01:57 -0300

    Cria arq condicionais.py e resolve questão 6

---
+++ condicionais.py
@@ -0,0 +1,10 @@
+# Questão 6
+def verificar_maioridade():
+    idade = int(input("Digite sua idade: "))
+
+    if (idade >= 18):
+        print("Maior de idade.")
+    else:
+        print("Menor de idade.")
+
+verificar_maioridade()
